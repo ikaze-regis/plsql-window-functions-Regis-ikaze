42fbaf34c0f1a143bdb7f779eb66381e634a429e
ikaze-regis <ikazeregis22@gmail.com>
2025-09-30 15:53:03 +0200

    Inserting values in created tables

---
+++ insertions.sql
@@ -0,0 +1,11 @@
+-- inserting in Table of Customers
+INSERT INTO customers VALUES 
+(101, 'Aline', 'Ngoma');
+
+-- inserting in Table of Products
+INSERT INTO products VALUES 
+(201, 'Beans', 'Proteins');
+
+-- inserting in Table of transactions
+INSERT INTO transactions VALUES 
+(301, 101, 201, DATE '2025-01-10', 25000);
